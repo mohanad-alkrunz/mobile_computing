832685c5e0e61cd4accb78658e31ccf6fa693a31
DELL <muhanadalkrunz@gmail.com>
2021-10-26 22:59:42 +0300

    Second commit add Binding dep.

--- app/src/main/java/com/mohanadalkrunz079/mobilecomputing/Splash_activity.java
+++ app/src/main/java/com/mohanadalkrunz079/mobilecomputing/Splash_activity.java
@@ -6,22 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.mohanadalkrunz079.mobilecomputing.databinding.ActivitySplashBinding;
+
 public class Splash_activity extends AppCompatActivity {
 
-
-    TextView go_next_page;
-
+    ActivitySplashBinding binding;
+    //#Created by mohanad alkrunz 26-Oct-2021 computing lab
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding  = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-
-        go_next_page= findViewById(R.id.go_next_page);
-
-        go_next_page.setOnClickListener(v->{
+        binding.goNextPage.setOnClickListener(v->{
             Intent intent = new Intent(this,MainActivity.class);
             startActivity(intent);
         });
+
     }
 }
